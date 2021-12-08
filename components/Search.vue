60c671e4158f52c2669f3a2d97555719f35d8dd8
yukikoshima <0gv3cj@gmail.com>
2021-12-08 12:55:33 +0900

    Search修正

--- components/Search.vue
+++ components/Search.vue
@@ -1,19 +1,23 @@
 <template>
-  <!-- append-outer-icon="mdi-magnify"
-    @click:append-outer="searchPoke" -->
-  <v-text-field
-    v-model="searchWord"
-    label="ポケモン検索"
-    placeholder="なまえやずかん番号でさがす"
-    hide-details
-    outlined
-    clearable
-    dense
-    type="text"
-    class="ml-md-12"
-    @keypress.enter="searchPoke"
-  >
-  </v-text-field>
+  <v-row>
+    <v-col cols="12" md="12" sm="6">
+      <v-text-field
+        v-model="searchWord"
+        label="ポケモン検索"
+        placeholder="なまえやずかん番号でさがす"
+        hide-details
+        outlined
+        clearable
+        dense
+        type="text"
+        class="ml-md-12"
+        append-outer-icon="mdi-magnify"
+        @click:append-outer="searchPoke"
+        @keypress.enter="searchPoke"
+      >
+      </v-text-field>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
